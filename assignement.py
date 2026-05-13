48427e752fce460781f69723a5bc9c9c42454f90
MahekSingh14 <maheksingh142005@gmail.com>
2026-05-13 16:08:37 +0530

    Added day1 problem & assignment

---
+++ assignement.py
@@ -0,0 +1,6 @@
+# Question- Take diameter as input and caculate the area of a circle 
+
+radius= int(input("Enter the radius of the circle :"))
+
+area_of_circle = 3.14*(radius**2)
+print("Area of the circle is :",area_of_circle)
---
+++ practice.py
@@ -0,0 +1,12 @@
+# Assignement Question : Print your name 10 time using * symbol
+print(("Mahek singh" + "\n") * 10)
+
+
+# Program to take input from the user
+
+print("A sample program to understand input method in python")
+
+name= input("Enter your good name :")
+age= input("Enter Your current age :")
+print("your good name is :", name)
+print("your current age is :", age)
